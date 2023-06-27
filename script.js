f44ac30d72e42ec8325084444a09deaf211466ed
Seya <31957516+sglkc@users.noreply.github.com>
2023-06-27 07:24:45 +0700

    feat: use cors proxy api
    feat: add fetch and error handling

--- script.js
+++ script.js
@@ -3,24 +3,32 @@ document
   .addEventListener('submit', (e) => {
     e.preventDefault();
 
+    const response = document.getElementById('response');
     const url = e.target[0].value;
 
-    fetch('https://api.allorigins.win/get?url=' + encodeURI(url))
-      .then(res => {
-        if (res.ok) return res.json();
-        document.getElementById('title').text = 'Failed to fetch page, try again later?';
+    response.removeAttribute('hidden');
+    response.innerText = 'Fetching page from url';
+
+    fetch('https://corsproxy.org/?' + encodeURIComponent(url))
+      .then((res) => {
+        if (res.ok) return res.text();
+        response.innerText = 'Failed to fetch page, try again later?';
       })
-      .then(res => {
-        const { contents } = res;
-        const [, title] = contents.match(/<title>([\S\s]+) - (音楽コラボアプリ nana|Music collab app 'nana')<\/title>/);
-        const [, sound_url] = contents.match(/sound_url=(\S+);.\.image/);
-        const [, image_url] = contents.match(/property="og:image" content="(\S+)">/);
+      .then((res) => {
+        const [, title] = res.match(/<title>([\S\s]+) - .+<\/title>/);
+        const [, sound_url] = res.match(/sound_url=(\S+);.\.image/);
+        const [, image_url] = res.match(/property="og:image" content="(\S+)">/);
         const sound = JSON.parse(sound_url);
 
+        response.setAttribute('hidden', true);
         document.getElementById('results').removeAttribute('hidden');
         document.getElementById('title').innerText = title;
         document.getElementById('link').innerText = sound;
         document.getElementById('audio').src = sound;
         document.getElementById('image').src = image_url;
+      })
+      .catch((err) => {
+        response.innerText = 'Error: ' + err;
+        console.error(err);
       });
   });
